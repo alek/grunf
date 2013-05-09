8645885303dbe591d69ac6348d1c428b65db9892
Aleksandar Bradic <abradic@sfadmins-MacBook-Pro-4.local>
2013-05-08 17:56:04 -0700

    add slf4j-nop dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
   	[org.clojure/clojure "1.5.1"]
+    [org.slf4j/slf4j-nop "1.7.2"]
   	[clj-http "0.4.1"]
 	  [clojurewerkz/quartzite "1.0.1"]
   ]
